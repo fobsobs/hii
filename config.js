c978681e917688a9c71d43de9bafd8a1814a9f3c
fobsobs <vvmizukii@gmail.com>
2025-07-13 20:16:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -68,9 +68,9 @@ const CONFIG = {
     // Adjust these if you want faster/slower animations
     animations: {
         floatDuration: "9s",           // How long it takes hearts to float up (10-20s recommended)
-        floatDistance: "40px",          // How far hearts move sideways (30-70px recommended)
-        bounceSpeed: "1s",            // Speed of bouncing animations (0.3-0.7s recommended)
-        heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
+        floatDistance: "60px",          // How far hearts move sideways (30-70px recommended)
+        bounceSpeed: "5s",            // Speed of bouncing animations (0.3-0.7s recommended)
+        heartExplosionSize: 1.0         // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
     // Background Music (Optional)
